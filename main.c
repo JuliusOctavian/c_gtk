291e7100eaa37afa8345d42c6f0ef8c8ed85558b
octavian <su30343537@outlook.com>
2022-03-24 23:30:36 +0800

    no ui

--- main.c
+++ main.c
@@ -1,70 +1,21 @@
-#include <stdio.h>
-#include <pthread.h>
 #include <gtk/gtk.h>
 
-int g_number = 0;
-
-#define MAX_COUNT 100
-
-pthread_t t1, t2;
-
-GtkApplicationWindow *window;
-GtkButton *l_btn, *m_btn, *r_btn;
-GtkLabel *label;
-
-void *counter(void *args) {
-    int j = 0;
-    char text[64];
-    while (j < MAX_COUNT) {
-        g_number++;
-        sprintf(text, "hi,i am pthread 2, my g_number is [%d]", g_number);
-        gtk_label_set_label(label, text);
-        usleep(1000 * 100);
-        j++;
-    }
-
-    gtk_widget_set_sensitive(GTK_WIDGET(l_btn), TRUE);
-}
-
-void multi_thread() {
-    pthread_create(&t2, NULL, counter, NULL);
-}
-
-void on_l_btn_click() {
-    gtk_label_set_label(label, "The matrix has you...");
-    gtk_widget_set_sensitive(GTK_WIDGET(l_btn), FALSE);
-
-    multi_thread();
-}
-
-void on_r_btn_click() {
-    if (pthread_cancel(t2) == 0) {
-        gtk_widget_set_sensitive(GTK_WIDGET(l_btn), TRUE);
-        gtk_label_set_label(label, "");
-
-        g_print("Thread canceled!\n");
-    }
-}
-
 void on_activate(GtkApplication *app) {
-    GtkBuilder *builder;
-    builder = gtk_builder_new_from_file("./resource/window.ui");
+    GtkWidget *window,*button, *label;
+    GtkBox *box;
 
-    window = GTK_APPLICATION_WINDOW(gtk_builder_get_object(builder, "window"));
-    l_btn = GTK_BUTTON(gtk_builder_get_object(builder, "l_btn"));
-    m_btn = GTK_BUTTON(gtk_builder_get_object(builder, "m_btn"));
-    r_btn = GTK_BUTTON(gtk_builder_get_object(builder, "r_btn"));
-    label = GTK_LABEL(gtk_builder_get_object(builder, "label"));
+    window = gtk_application_window_new(app);
 
-    gtk_application_add_window(app, GTK_WINDOW(window));
+    box = GTK_BOX(gtk_box_new(GTK_ORIENTATION_VERTICAL, 30));
+    gtk_box_set_homogeneous(box, TRUE);
 
-    g_signal_connect(l_btn, "clicked", G_CALLBACK(on_l_btn_click), NULL);
-    g_signal_connect_swapped(m_btn, "clicked", G_CALLBACK(gtk_window_close), window);
-    g_signal_connect(r_btn, "clicked", G_CALLBACK(on_r_btn_click), NULL);
+    button = gtk_button_new_with_label("Click");
+    label = gtk_label_new("");
 
-    gtk_window_present(GTK_WINDOW(window));
+    gtk_box_append(box, button);gtk_box_append(box, label);
+    gtk_window_set_child(GTK_WINDOW(window), GTK_WIDGET(box));
 
-    g_object_unref(builder);
+    gtk_widget_show(window);
 }
 
 int main(int argc, char *argv[]) {
